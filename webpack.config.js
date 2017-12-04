f6f2ac380332781550bb5a233a05965e4d4bd6c4
李良知 <296452017@qq.com>
2017-12-04 23:09:36 +0800

    use eslint and autoprefix for css

--- webpack.config.js
+++ webpack.config.js
@@ -4,74 +4,151 @@ const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const pathResolve = (dir) => {
+  return path.join(__dirname, dir)
+}
+
 let entries = getEntry('src/js/*.js')
 let chunks = Object.keys(entries)
  
 let config = {
-    entry: entries,
-    output: {
-        path: path.join(__dirname, 'dist'),
-        publicPath: '/dist/',
-        filename: 'js/[name].js',
-        chunkFilename: 'js/[id].chunk.js?[chunkhash]'
-    },
-    module: {
-        loaders: [
-                {
-                    test: /\.css$/,
-                    loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
-                },{
-                    test: /\.less$/,
-                    loader: ExtractTextPlugin.extract('css-loader!less-loader')
-                },{
-                    test: /\.html$/,
-                    loader: 'html-loader?attrs=img:src img:data-src'
-                },{
-                    test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                    loader: 'file-loader?name=./fonts/[name].[ext]'
-                },{
-                    test: /\.(png|jpg|gif)$/,
-                    loader: 'url-loader?limit=8192&name=img/[hash].[ext]'
-                },{
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader'
-                }
-        ]
-    },
-    plugins: [
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                'wx': 'weixin-js-sdk',
-                'weui': 'weui.js'
-            }),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'vendors',
-                chunks: chunks,
-                minChunks: 1
-            }),
-            new ExtractTextPlugin('css/[name].css'),
-            // new HtmlWebpackPlugin({
-            //     favicon: './src/img/favicon.ico',
-            //     filename: './view/index.html',
-            //     template: './src/view/index.html',
-            //     indect: 'body',
-            //     hash: true,
-            //     chunks: ['vendors', 'index'],
-            //     minify: {
-            //         removeComments: true,
-            //         collapseWhitespace: false
-            //     }
-            // }),
-            new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-        contentBase: './dist/view/',
-        host: '0.0.0.0',
-        port: 4500,
-        inline: true,
-        hot: true
+  devtool: 'eval-source-map',
+  entry: entries,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'js/[name].js',
+    chunkFilename: 'js/[id].chunk.js?[chunkhash]'
+  },
+  resolve: {
+    alias: {
+      'src': pathResolve('src'),
+      'common': pathResolve('src/common'),
+      'libs': pathResolve('src/libs')
     }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: pathResolve('src'),
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
+      {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: 'css-loader'
+          })
+      },{
+          test: /\.less$/,
+          use: ExtractTextPlugin.extract({
+            use: [
+              {
+                loader: 'css-loader'
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-import')(),
+                    require('postcss-flexbugs-fixes'),
+                    require('autoprefixer')({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9',
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                }
+              },
+              {
+                loader: 'less-loader'
+              }
+            ]
+          })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: [':data-src', 'img:src']
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: './fonts/[name].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: 'img/[hash].[ext]'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      'wx': 'weixin-js-sdk',
+      'weui': 'weui.js'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendors',
+      chunks: chunks,
+      minChunks: 1
+    }),
+    new ExtractTextPlugin('css/[name].css'),
+    // new HtmlWebpackPlugin({
+    //     favicon: './src/img/favicon.ico',
+    //     filename: './view/index.html',
+    //     template: './src/view/index.html',
+    //     indect: 'body',
+    //     hash: true,
+    //     chunks: ['vendors', 'index'],
+    //     minify: {
+    //         removeComments: true,
+    //         collapseWhitespace: false
+    //     }
+    // }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist/view/',
+    host: '0.0.0.0',
+    port: 4500,
+    inline: true,
+    hot: true
+  }
 }
 let pages = Object.keys(getEntry('src/view/*.html'))
 
